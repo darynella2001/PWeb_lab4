44d191bf4be69001ecbc54b777699b8f8492bb96
Darinela Andronovici <darynella2001@gmail.com>
2022-04-12 14:54:48 +0300

    create quiz and submit it

--- src/Test.js
+++ src/Test.js
@@ -1,8 +1,14 @@
 import React, {useState} from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const Test = () => {
     const [formFields, setFormFields] = useState([{question:'',answers:[],correct_answer:''},])
     const [title, setTitle] = useState('');
+    const [submission, setSubmission] = useState({data:{}})
+    const navigate = useNavigate();
+
+    
     
     const handleFormChange = (event, index) => {
         let data = [...formFields];
@@ -26,7 +32,27 @@ const Test = () => {
       const submit = (e) => {
         e.preventDefault();
         console.log(formFields)
+        setSubmission(submission.data = {title, "questions":formFields})
+        console.log(submission);
+
+        axios.post('https://pure-caverns-82881.herokuapp.com/api/v54/quizzes', submission,  
+        {headers:{
+                        "X-Access-Token": '00e42cabcc70501b0941ae177326c2c7407e96d701a97a82d97577d4db27e6de',
+                    }
+                })
+        .then((res) => {
+            // console.log(res.data.id)
+            // console.log(postData)
+            // postData.data.id = res.data.id
+            // console.log(postData)
+            // localStorage.setItem("user-info", JSON.stringify(postData["data"]))
+            navigate("/about")
+        })
+        .catch((err)=>{
+            console.log(err)            
+        })
       }
+    
   
       const addFields = () => {
         let object = {
@@ -50,51 +76,46 @@ const Test = () => {
                 <div className="card">
                     <div className="card-body">
                         <form onSubmit={submit}>
+                            <label htmlFor="title" className="form-label">Title</label> <br/>
+                            <input type="text"  className="form-control" name="title" value={title} onChange={(e)=>setTitle(e.target.value)}/>
+                            <br/>
                             { 
                             formFields.map((form, index) => {
                                 return(
                                     <div key={index}>
-                                        <input type="text" name="question"
-                                                placeholder="question"
-                                                onChange={event=>handleFormChange(event, index)}
-                                                value={form.question}
-                                                />
-                                                <input type="text" name="answers[0]"
-                                                placeholder="answer1"
-                                                onChange={event=>handleFormChange(event, index)}
-                                                value={form.answers[0]}
-                                                />
-                                                <input type="text" name="answers[1]"
-                                                placeholder="answer2"
-                                                onChange={event=>handleFormChange(event, index)}
-                                                value={form.answers[1]}
-                                                />
-                                                <input type="text" name="answers[2]"
-                                                placeholder="answer3"
-                                                onChange={event=>handleFormChange(event, index)}
-                                                value={form.answers[2]}
-                                                />
-                                                <input type="text" name="answers[3]"
-                                                placeholder="answer4"
-                                                onChange={event=>handleFormChange(event, index)}
-                                                value={form.answers[3]}
-                                                />
-                                                <select name="correct_answer" value={form.correct_answer} onChange={event=>handleFormChange(event, index)}>
-                                                    <option defaultValue value={form.answers[0]}>{form.answers[0]}</option>
-                                                    <option value={form.answers[1]}>{form.answers[1]}</option>
-                                                    <option value={form.answers[2]}>{form.answers[2]}</option>
-                                                    <option value={form.answers[3]}>{form.answers[3]}</option>
-                                                </select>
-                                                <button onClick={() => removeFields(index)}>Remove</button>
+                                        <label htmlFor="title" className="form-label">Questions</label> 
+                                        <input type="text"  className="form-control" name="question" onChange={event=>handleFormChange(event, index)} value={form.question}/> <br/>
 
+                                        <label htmlFor="answer[0]" className="form-label">Answer 1</label> 
+                                        <input type="text" className="form-control" name="answers[0]" placeholder="answer1" onChange={event=>handleFormChange(event, index)} value={form.answers[0]}/> <br/>
+
+                                        <label htmlFor="answer[1]" className="form-label">Answer 2</label>         
+                                        <input type="text" className="form-control" name="answers[1]" onChange={event=>handleFormChange(event, index)} value={form.answers[1]}/> <br/>
+
+                                        <label htmlFor="answer[2]" className="form-label">Answer 3</label>         
+                                        <input type="text" className="form-control" name="answers[2]" onChange={event=>handleFormChange(event, index)} value={form.answers[2]}/> <br/>
+
+                                        <label htmlFor="answer[3]" className="form-label">Answer 4</label> <br/>        
+                                        <input type="text" className="form-control" name="answers[3]" onChange={event=>handleFormChange(event, index)} value={form.answers[3]}/> <br/>
+                                         
+                                        <label htmlFor="correct_answer" className="form-label">Select the correct answer:</label> <br/>        
+                                        <select name="correct_answer" className="form-control" value={form.correct_answer} onChange={event=>handleFormChange(event, index)}>
+                                            <option defaultValue value={form.answers[0]}>{form.answers[0]}</option>
+                                            <option value={form.answers[1]}>{form.answers[1]}</option>
+                                            <option value={form.answers[2]}>{form.answers[2]}</option>
+                                            <option value={form.answers[3]}>{form.answers[3]}</option>
+                                        </select>
+                                        <br/>
+                                        <button className="btn btn-danger mb-3" onClick={() => removeFields(index)}>Remove One Question </button>
                                     </div>
                                 )
                             })
                             }
                         </form>
-                        <button onClick={addFields}>Add More..</button>
-                                  <button onClick={submit}>Submit</button>
-
+                        
+                            <button className="btn btn-info mb-3" onClick={addFields}>Add One Question</button> <br/>
+                            <button className="btn btn-success" onClick={submit}>Submit Quiz</button>
+                        
                     </div>
 
                 </div>
